141ab34aabb42bac62404743da7de9fdf7a89c82
unknown <luandeoliveirasoares@gmail.com>
2023-09-25 21:36:39 -0300

    wip: building layout wireframe

--- src/app/components/menu/menu.component.ts
+++ src/app/components/menu/menu.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
   styleUrls: ['./menu.component.css']
 })
-export class MenuComponent {
 
+
+
+
+export class MenuComponent implements OnInit {
+  ngOnInit(): void {
+    // console.log('oi')
+    
+    // throw new Error('Method not implemented.');
+
+  }
 }
--- src/app/components/small-card/small-card.component.ts
+++ src/app/components/small-card/small-card.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-small-card',
   templateUrl: './small-card.component.html',
   styleUrls: ['./small-card.component.css']
 })
-export class SmallCardComponent {
+export class SmallCardComponent implements OnInit {
+  ngOnInit(): void {
+    console.log('oi')
+    
+    // throw new Error('Method not implemented.');
 
+  }
 }
